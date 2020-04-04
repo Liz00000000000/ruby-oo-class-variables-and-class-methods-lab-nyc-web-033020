093b8142f223fd618ef5456a90bb899f81bd27af
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-04-04 21:27:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,14 @@
 class Song
   attr_reader :name, :artist, :genre
   @@all = [ ]
+  @@artists = [ ]
   @@count = 0
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
-    @@artists << self.artist 
+    @@artists << self.artist
     Song.all << self
   end
 
